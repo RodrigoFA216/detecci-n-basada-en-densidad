d8bdabde7e80ddc39e93812beb158c89428e0f39
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2023-02-12 20:16:00 -0600

    do more than only one

---
+++ density_and_tree.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+# Generar datos sintéticos
+np.random.seed(42)
+X = 0.3 * np.random.randn(100, 2)
+X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+X = np.r_[X + 2, X - 2, X_outliers]
+y = np.concatenate([np.ones(100), np.zeros(20)])
+
+# Ajustar el modelo de detección de outliers
+clf = LocalOutlierFactor(n_neighbors=20)
+y_pred = clf.fit_predict(X)
+
+# Separar los datos en outliers y no outliers
+X_train, X_test, y_train, y_test = train_test_split(X[y_pred == 1], y[y_pred == 1], test_size=0.33, random_state=42)
+
+# Ajustar un árbol de decisión en los datos no outliers
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+
+# Evaluar el rendimiento del árbol de decisión
+print("Precisión en los datos de entrenamiento:", dt.score(X_train, y_train))
+print("Precisión en los datos de prueba:", dt.score(X_test, y_test))
--- detect_density.py
+++ detect_density.py
@@ -8,7 +8,7 @@ X = 0.3 * np.random.randn(100, 2) # genera datos de 100 filas por dos columnas
 #genera datos que funcionarán de outliers entre -4 y 4 de 20 filas y dos columnas
 X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2)) 
 #concatenamos en x las matrices que ya habíamos hecho, 
-# en primer lugar concatenamos x desplazado en 2 y x desplazado en -2 (en y) y concatenamos los outliers 
+# en primer lugar concatenamos x desplazado en 2 y x desplazado en -2 (en x y y) y concatenamos los outliers 
 # esto va a generar una dispersión atípica de los valores outliers
 # no importa el orden en que sean concatenados mientras estén en un solo objeto
 X = np.r_[X + 2, X - 2, X_outliers]
@@ -19,12 +19,25 @@ X = np.r_[X + 2, X - 2, X_outliers]
 # con la densidad de los vecinos de los demás puntos
 # La variable n_neighbors se utiliza para especificar el número de vecinos más cercanos que 
 # se utilizarán para calcular la densidad
-clf = LocalOutlierFactor(n_neighbors=20)
+clf = LocalOutlierFactor(n_neighbors=10)
+# se ajusta el modelo de detección dando como parámetros el conjunto X que 
+# recordando el conjunto original se transformó en x+2 y x-2 dentro de x
 y_pred = clf.fit_predict(X)
 
 # Graficar los resultados
+#se crea un objeto colors donde se etiquetarán de azul 377eb8 los outliers y de 
+# naranja ff7f00los datos que son representativos
 colors = np.array(['#377eb8', '#ff7f00'])
+# Grafica primera columna en x y segunda columna en Y
+# usa colors para asignar colores a los puntos y_pred contiene las 
+# etiquetas de predicción devueltas por el modelo LocalOutlierFactor
 plt.scatter(X[:, 0], X[:, 1], color=colors[(y_pred + 1) // 2])
+# La expresión color=colors[(y_pred + 1) // 2] se utiliza para asignar un color a 
+# cada punto en función de su etiqueta de predicción. 
+# Si un punto es considerado un outlier (y_pred = -1), se le asigna el primer color de 
+# la lista colors, y si no es un outlier (y_pred = 1), se le asigna el segundo color. 
+# La expresión (y_pred + 1) // 2 se utiliza para mapear los valores de y_pred a los 
+# índices de los colores en la lista
 plt.xlim((-5, 5))
 plt.ylim((-5, 5))
 plt.show()
